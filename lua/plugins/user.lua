d11ff6476398259f6d14b5d1f68a50d55b079455
Janusz Janik <janusz.janik@hotmail.com>
2025-11-03 19:55:41 +0100

    fix first run

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,3 +1,18 @@
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    local ok, mason_registry = pcall(require, "mason-registry")
+    if ok then
+      -- odczekaj sekundę, aż Mason skończy instalacje w tle
+      vim.defer_fn(function()
+        if mason_registry.refresh then
+          mason_registry.refresh()
+        end
+      end, 1000)
+    end
+  end,
+})
+
 return {
   {
     "LazyVim/LazyVim",
@@ -7,36 +22,10 @@ return {
     end,
   },
 
-  -- Mason (menedżer LSP, DAP, narzędzi)
-  {
-    "mason-org/mason.nvim",
-    build = ":MasonUpdate",
-    config = true,
-  },
-
-  -- Mason LSP bridge + Rust analyzer
   {
     "mason-org/mason-lspconfig.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
     opts = {
-      ensure_installed = { "rust_analyzer" },
-    },
-  },
-
-  -- Konfiguracja LSP dla Rust
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        rust_analyzer = {
-          settings = {
-            ["rust-analyzer"] = {
-              cargo = { allFeatures = true },
-              checkOnSave = { command = "clippy" },
-            },
-          },
-        },
-      },
+      ensure_installed = { "rust-analyzer" },
     },
   },
 }
