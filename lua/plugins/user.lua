13de706663b6966c9c337c4f5a4c153fac8fd676
Janusz Janik <janusz.janik@hotmail.com>
2025-11-03 19:48:58 +0100

    fix rust-analyzer installation

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -7,10 +7,36 @@ return {
     end,
   },
 
+  -- Mason (menedżer LSP, DAP, narzędzi)
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = true,
+  },
+
+  -- Mason LSP bridge + Rust analyzer
   {
     "mason-org/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
     opts = {
       ensure_installed = { "rust_analyzer" },
+    },
+  },
+
+  -- Konfiguracja LSP dla Rust
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = { allFeatures = true },
+              checkOnSave = { command = "clippy" },
+            },
+          },
+        },
+      },
     },
   },
 }
