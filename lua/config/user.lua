ffbd95543f78c46358e6d87989e92ee82817cae3
Janusz Janik <janusz.janik@hotmail.com>
2025-11-03 18:58:40 +0100

    reorganize plugins

--- lua/config/user.lua
+++
@@ -1,8 +0,0 @@
--- ~/.config/nvim/lua/config/user.lua
-
--- Twoje osobiste ustawienia
-vim.schedule(function()
-  vim.cmd.colorscheme("industry")
-end)
-
-vim.opt.relativenumber = false
---
+++ lua/plugins/user.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    config = function()
+      vim.opt.relativenumber = false
+      vim.cmd.colorscheme("industry")
+    end,
+  },
+}
